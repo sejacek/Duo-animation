116deccfc31b49ad799883aa160685e6ca3d2ce6
sejacek <jacek@mactelo.pl>
2026-09-12 11:27:20 +0200

    Update import for Modifier in DemoContentView.kt

--- app/src/main/java/com/example/duofold/ui/DemoContentView.kt
+++ app/src/main/java/com/example/duofold/ui/DemoContentView.kt
@@ -2,7 +2,7 @@ package com.example.duofold.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Modifier
+import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.layout.ContentScale
